2784b69b21f2241d014a3293e8fe5fe18c94620e
GustavNordgren95 <112472606+GustavNordgren95@users.noreply.github.com>
2025-03-22 14:25:04 +0100

    Location can be retrieved via API

---
+++ src/WeatherApp.java
@@ -0,0 +1,89 @@
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+
+import java.net.HttpURLConnection;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class WeatherApp {
+    public static JSONObject getWeatherData(String locationName){
+        // get location coordinates using the geolocation API
+        JSONArray locationData = getLocationData(locationName);
+
+        return null;
+    }
+
+    // retrieves geographic coordinates for given location name
+    public static JSONArray getLocationData(String locationName){
+        // replace any whitespace in location name to + to adhere to API's request format
+        locationName = locationName.replaceAll(" ", "+");
+
+        // build API url with location parameter
+        String urlString = "https://geocoding-api.open-meteo.com/v1/search?name=" +
+                locationName + "&count=10&language=en&format=json";
+
+        try{
+            // call api and get a response
+            HttpURLConnection conn = fetchApiResponse(urlString);
+
+            // check response status
+            // 200 means successful connection
+            if(conn.getResponseCode() != 200){
+                System.out.println("Error: Could not connect to API");
+                return null;
+            }else{
+                // store the API results
+                StringBuilder resultJson = new StringBuilder();
+                Scanner scanner = new Scanner(conn.getInputStream());
+
+                // read and store the resulting json data into our string builder
+                while(scanner.hasNext()){
+                    resultJson.append(scanner.nextLine());
+                }
+
+                // close scanner
+                scanner.close();
+
+                // close url connection
+                conn.disconnect();
+
+                // parse the JSON string into a JSON obj
+                JSONParser parser = new JSONParser();
+                JSONObject resultsJsonObj = (JSONObject) parser.parse(String.valueOf(resultJson));
+
+                // get the list of location data the API gtenerated from the lcoation name
+                JSONArray locationData = (JSONArray) resultsJsonObj.get("results");
+                return locationData;
+            }
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        // couldn't find location
+        return null;
+    }
+
+    private static HttpURLConnection fetchApiResponse(String urlString){
+        try{
+            // attempt to create connection
+            URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            // set request method to get
+            conn.setRequestMethod("GET");
+
+            // connect to our API
+            conn.connect();
+            return conn;
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+        // could not make connection
+        return null;
+    }
+
+}
